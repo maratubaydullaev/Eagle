fa054fafcc633f067d9ec829439b16826b4e92f7
maratubaydullaev <marat.ubaydullaev@icloud.com>
2026-09-19 01:02:26 +0500

    fix: harden offline service worker

--- public/sw.js
+++ public/sw.js
@@ -1,1 +1,11 @@
-const CACHE='pochemuchka-v1';self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['/','/manifest.webmanifest']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).catch(()=>caches.match('/')))));
+const CACHE='pochemuchka-shell-v2'
+const SHELL=['/','/manifest.webmanifest','/icons/icon-192.svg','/icons/icon-512.svg']
+self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting())))
+self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())))
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET') return
+  event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{
+    if(response.ok&&new URL(event.request.url).origin===self.location.origin){const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy))}
+    return response
+  }).catch(()=>caches.match('/'))))
+})
