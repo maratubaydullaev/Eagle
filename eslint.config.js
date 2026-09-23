cf1a7612d714a50783d7e72cd4d9bf0ff7ec75f5
maratubaydullaev <marat.ubaydullaev@icloud.com>
2026-09-24 03:22:38 +0500

    Add ESLint flat config

---
+++ eslint.config.js
@@ -0,0 +1,15 @@
+import js from '@eslint/js'
+import tseslint from 'typescript-eslint'
+
+export default tseslint.config(
+  { ignores: ['dist', 'node_modules', 'supabase', 'e2e', 'public'] },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/no-non-null-assertion': 'off',
+    },
+  },
+)
